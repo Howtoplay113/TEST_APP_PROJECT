8405dda68f383d4cc7820eb957376ffc76f96e2e
Roman <168887998+Howtoplay113@users.noreply.github.com>
2025-03-25 10:16:26 +0300

    Добавление кнопки и реакции на нажатие кнопки

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 #Первый файл тестового проекта по созданию приложения на андроиде. 20-03-2025
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return Label(text=f"Привет, это мое первое тестове приложение (пока без кнопочек)!\nР.С. 25.03.2025")
+        layout = BoxLayout(orientation='vertical')
+        self.label = Label(text="Нажми кнопку")
+        button = Button(text="Нажми меня")
+        button.bind(on_press=self.on_button_press)
+        layout.add_widget(self.label)
+        layout.add_widget(button)
+        return layout
+
+    def on_button_press(self, instance):
+        self.label.text = "Кнопка нажата!"
 
 if __name__ == "__main__":
     MyApp().run()
