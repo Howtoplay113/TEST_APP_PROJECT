d3a6594d29f69cf4a50cc374f09ebaf2bc6306ba
Roman <168887998+Howtoplay113@users.noreply.github.com>
2025-03-25 10:12:12 +0300

    Это первая версия первого тестового приложения Р.С. 25-03-2025 - один экран без кнопок

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #Первый файл тестового проекта по созданию приложения на андроиде. 20-03-2025
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text=f"Привет, это мое первое тестове приложение (пока без кнопочек)!\nР.С. 25.03.2025")
+
+if __name__ == "__main__":
+    MyApp().run()
